f0ba3b13aaadc03e08f70252442bd5509fab348f
sudi <saudi33gg@gmail.com>
2024-11-14 15:54:42 +0200

    Home Page API's

--- src/app/modules/general/pages/about/about.component.ts
+++ src/app/modules/general/pages/about/about.component.ts
@@ -8,6 +8,27 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   expandedIndex: number | null = null;
 
+  TeamSlider = {
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+      centeredSlides: true,
+      loop: true,
+    },
+    500: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  };
+
   toggleExpand(index: number): void {
     this.expandedIndex = this.expandedIndex === index ? null : index;
   }
