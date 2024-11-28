b97daa77e010c3c89ea454cdd7a7c2ea477e8119
sudi <saudi33gg@gmail.com>
2024-11-29 00:41:48 +0200

    Project details dynamic content

--- src/app/modules/general/components/project-details/project-details.component.ts
+++ src/app/modules/general/components/project-details/project-details.component.ts
@@ -33,47 +33,7 @@ export class ProjectDetailsComponent implements OnInit {
     private _SiteContentService: SiteContentService,
     private translate: TranslateService
   ) {
-    this.events = [
-      {
-        status: 'يوم التأسيس السعودي',
-        description: '  23 - 9 - 2023   ',
-      },
-      {
-        status: 'نظرة عامة على المشروع',
-        description:
-          'تم تكليفنا بتنظيم حدث ضخم بمناسبة "يوم التأسيس السعودي"، والذي يعكس تراث المملكة وثقافتها الغنية. تمحور التحدي حول تصميم وتنفيذ تجربة متكاملة، تشمل العروض المرئية والتقنيات الحديثة، التي تعبر عن الهوية الوطنية.',
-      },
-      {
-        status: 'النهج الذي اتبعناه',
-        description:
-          'بدأنا بتطوير خطة تصميم متكاملة بالتعاون مع العميل لتجسيد رؤية "يوم التأسيس". تضمنت العملية تصميم الديكورات التراثية، إعدادات المسرح، استخدام الشاشات الرقمية، والتقنيات الحديثة مثل الإسقاط المرئي (projection mapping) لتجسيد التاريخ والرموز الوطنية.',
-      },
-      {
-        status: 'أهم الميزات',
-        description: `تصاميم الديكور:
-تم تصميم الديكورات بأسلوب مستوحى من التراث السعودي، باستخدام الزخارف والألوان الوطنية.
-
-الإسقاط المرئي:
-استخدمنا تقنية الإسقاط المرئي لعرض لقطات من تاريخ المملكة على جدران المكان، مما أضفى طابعًا مميزًا على الحدث.
-
-الأداء التفاعلي:
-تضمن الحدث عروضًا حية ومسرحية تحكي قصة تأسيس المملكة، بمشاركة فرق محلية ودولية.`,
-      },
-      {
-        status: 'النتائج',
-        description:
-          'حقق الحدث نجاحًا كبيرًا، حيث حضره أكثر من [عدد الحضور] شخص، وتمت تغطيته إعلاميًا بشكل واسع في وسائل الإعلام المحلية والدولية. استطاع الحدث ترك بصمة قوية لدى الجمهور، وتم الاعتراف به كواحد من أفضل الفعاليات للاحتفال بالتراث السعودي.',
-      },
-      {
-        status: 'شهادة العميل',
-        description: '',
-      },
-    ];
-
-    this.eventsWithIndex = this.events.map((event, index) => ({
-      ...event,
-      index,
-    }));
+    this.initialize();
   }
 
   @ViewChild('testimonialSwiper') testimonialSwiper!: ElementRef;
@@ -88,14 +48,37 @@ export class ProjectDetailsComponent implements OnInit {
 
   activeIndex: number = 0;
 
-  events: any[];
-  eventsWithIndex: any[];
+  events!: any[];
+  eventsWithIndex!: any[];
 
   ngOnInit(): void {
+    this.initialize();
+  }
+
+  initialize(): void {
     this._SiteContentService.getProjectDetails(this.projectId).subscribe({
       next: (data) => {
         this.project = data.data;
-        console.log(this.project);
+
+        this.events = data.data.details;
+        // Add the last object
+
+        if (this.direction == 'ltr') {
+          this.events.push({
+            title: 'Customer Testimonial',
+            description: '',
+          });
+        } else {
+          this.events.push({
+            title: 'شهادة العميل',
+            description: '',
+          });
+        }
+
+        this.eventsWithIndex = this.events.map((event: any, index: any) => ({
+          ...event,
+          index,
+        }));
       },
     });
 
